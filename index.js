16c8b4486b776ae826f6dacd94b4291f257b1423
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-04-06 19:23:12 -0400

    updates to sub stream

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const comments = s.Stream("comment", {
 app.get("/", (_req, res) => {
   console.log("endpoint hit...");
 
-  r.getDefaultSubreddits("cooking").then((item) => {
+  r.getSubreddit("cooking").then((item) => {
     console.log(item);
     res.send(item);
   });
