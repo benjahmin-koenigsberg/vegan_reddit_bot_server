53d136d8372b35ea67509c5eb2094f06d59c33ab
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-04-06 10:29:12 -0400

    add imports

--- index.js
+++ index.js
@@ -2,12 +2,22 @@
 
 const express = require("express");
 const app = express();
+const snoowrap = require("snoowrap");
+const snoostorm = require("snoostorm-es6");
+const { InboxStream } = require("snoostorm");
 
 var cors = require("cors");
 app.use(cors());
 
+const dotenv = require("dotenv").config();
+
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+let io;
+
 app.get("/", (req, res) => {
-  res.send("a-ok!");
+  res.send("a-ok still works!");
 });
 
 app.listen(process.env.PORT || 8001, () => {
