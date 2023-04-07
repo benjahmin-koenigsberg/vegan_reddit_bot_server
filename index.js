b607c3c3dc1787f53cc87329bf3677985648bdd6
Benjahmin Koenigsberg <magnanimousplayer@gmail.com>
2023-04-06 21:43:07 -0400

    debug sub grab

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const comments = s.Stream("comment", {
 app.get("/", (_req, res) => {
   console.log("endpoint hit...");
 
-  r.getDefaultSubreddits("all+food+cooking+foodporn+bacon").then((item) => {
+  r.getDefaultSubreddits("all").then((item) => {
     console.log(item);
     res.send(item);
   });
